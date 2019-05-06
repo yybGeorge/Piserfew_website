c52ebc24924c5fcb2a803f55ce7ffae53051b132
yybGeorge <yy2580@nyu.edu>
2019-05-06 00:13:09 -0400

    111

---
+++ www/orm.py
@@ -0,0 +1,23 @@
+import asyncio, logging, aiomysql
+
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+
+    )
